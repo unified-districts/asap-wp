07e90160816136c742bb1e9b7d9d9ffbd633b415
Aidan Jones <aidan@aijo.info>
2021-04-18 15:07:16 -0700

    Removed pre-set directory from wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -45,4 +45,3 @@ $table_prefix  = 'wp_';
 // For developers: WordPress debugging mode
 // ========================================
 define('WP_DEBUG', false);
-if ( !defined('ABSPATH') ) define('ABSPATH', __DIR__ . '/'); require_once(ABSPATH . 'wp-settings.php');
